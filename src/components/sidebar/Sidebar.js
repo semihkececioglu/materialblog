2e297331b92b5554cd2b886da43f160db1721306
Semih Kececioglu <semihkecec@gmail.com>
2025-04-22 02:19:52 +0300

    sidebar temizlendi.

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,24 +1,9 @@
 import React from "react";
-import {
-  Typography,
-  Divider,
-  List,
-  ListItem,
-  ListItemText,
-  Paper,
-  useTheme,
-  Box,
-} from "@mui/material";
-import { Link } from "react-router-dom";
 import SocialMediaBox from "./SocialMediaBox";
 import CategoryList from "./CategoryList";
 import SidebarTags from "./SidebarTags";
 
-const categories = ["React", "JavaScript", "Tasarım"];
-
 const Sidebar = () => {
-  const theme = useTheme();
-
   return (
     <>
       <CategoryList />
