08bba30038509fd991ea32efb3c440c98c84e371
Semih Kececioglu <semihkecec@gmail.com>
2025-04-22 01:22:25 +0300

    sidebar.js optimize edildi.

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -11,6 +11,7 @@ import {
 } from "@mui/material";
 import { Link } from "react-router-dom";
 import SocialMediaBox from "./SocialMediaBox";
+import CategoryList from "./CategoryList";
 
 const categories = ["React", "JavaScript", "Tasarım"];
 
@@ -19,37 +20,7 @@ const Sidebar = () => {
 
   return (
     <>
-      <Paper
-        elevation={3}
-        sx={{
-          p: 2,
-          borderRadius: 2,
-          bgcolor: theme.palette.mode === "dark" ? "grey.900" : "grey.50",
-          border: `1px solid ${
-            theme.palette.mode === "dark"
-              ? theme.palette.grey[800]
-              : theme.palette.grey[300]
-          }`,
-        }}
-      >
-        <Typography variant="h6" gutterBottom>
-          Kategoriler
-        </Typography>
-        <Divider sx={{ my: 1 }} />
-        <List dense>
-          {categories.map((category) => (
-            <ListItem
-              key={category}
-              button
-              component={Link}
-              to={`/category/${category.toLowerCase()}`}
-            >
-              <ListItemText primary={category} />
-            </ListItem>
-          ))}
-        </List>
-      </Paper>
-
+      <CategoryList />
       <SocialMediaBox />
     </>
   );
