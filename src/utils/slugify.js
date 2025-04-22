9bce1d917c723065327ed7ba61891ad148d252f5
Semih Kececioglu <semihkecec@gmail.com>
2025-04-22 13:12:22 +0300

    post url olusturmak icin eklendi

---
+++ src/utils/slugify.js
@@ -0,0 +1,19 @@
+// utils/slugify.js
+
+const slugify = (text) => {
+  return text
+    .toString()
+    .toLowerCase()
+    .trim()
+    .replace(/ğ/g, "g")
+    .replace(/ü/g, "u")
+    .replace(/ş/g, "s")
+    .replace(/ı/g, "i")
+    .replace(/ö/g, "o")
+    .replace(/ç/g, "c")
+    .replace(/[^a-z0-9\s-]/g, "")
+    .replace(/\s+/g, "-")
+    .replace(/-+/g, "-");
+};
+
+export default slugify;
