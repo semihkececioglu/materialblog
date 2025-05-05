4a2e96a0cc3f8c1d356dd3dad3fed325e693b7fa
Semih Kececioglu <semihkecec@gmail.com>
2025-05-05 13:53:57 +0300

    index.js dosyasi guncellendi

--- server/index.js
+++ server/index.js
@@ -1,3 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-1;
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
