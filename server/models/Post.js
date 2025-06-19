b718a435b8d93156c25def24f1a3fa5504664535
semihkececioglu <semihkecec@gmail.com>
2025-06-19 16:31:48 +0300

    post to backend

---
+++ server/models/Post.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: String,
+  category: String,
+  tags: [String],
+  author: String,
+  date: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Post", postSchema);
