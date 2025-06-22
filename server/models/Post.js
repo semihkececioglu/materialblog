5837b1a9662c802c84b1970498d025b3ce428205
semihkececioglu <semihkecec@gmail.com>
2025-06-22 22:26:16 +0300

    db image added

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,12 +1,16 @@
 const mongoose = require("mongoose");
 
 const postSchema = new mongoose.Schema({
-  title: { type: String, required: true },
+  title: String,
+  summary: String,
   content: String,
   category: String,
   tags: [String],
-  author: String,
-  date: { type: Date, default: Date.now },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  image: String,
 });
 
 module.exports = mongoose.model("Post", postSchema);
