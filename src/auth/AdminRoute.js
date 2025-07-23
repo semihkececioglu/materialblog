34be85496660bb2367d5314dcf0ff99b58939695
semihkececioglu <semihkecec@gmail.com>
2025-07-23 14:00:46 +0300

    unneccessary comment line deleted

--- src/auth/AdminRoute.js
+++ src/auth/AdminRoute.js
@@ -5,7 +5,6 @@ import { useSelector } from "react-redux";
 const AdminRoute = ({ children }) => {
   const user = useSelector((state) => state.user.currentUser);
 
-  // Giriş yapılmamışsa
   if (!user) {
     return <Navigate to="/login" replace />;
   }
@@ -15,7 +14,6 @@ const AdminRoute = ({ children }) => {
     return <Navigate to="/" replace />;
   }
 
-  // <Route element={<AdminRoute />} /> veya <AdminRoute><X /></AdminRoute> uyumlu
   return children || <Outlet />;
 };
 
